a5e82e846f16e24750d54fb0b259ab0f66e1d70d
Mark Nano <78907926+marknano009@users.noreply.github.com>
2021-06-02 08:51:47 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"<b>{query}</b>\n<i>({(len_results)} Results)</i>\n\n⭕️ <i>If you didn't get required movie pls contact us</i> <b><a href='http://t.me/Malayalam_Movees_Bot'>👉 CLICH HERE 👈</a></b>\n\n<b>🛑You must Join Channels to access the links given below🛑\n\n<b>@Malayalam_Movees ©\n@Film_Caffe © (Series)</b>",
+                text=f"<b>{query}</b>\n<i>({(len_results)} Results)</i>\n\n⭕️ <i>If you didn't get required movie pls contact us</i> <b><a href='http://t.me/Malayalam_Movees_Bot'>👉 𝗖𝗹𝗶𝗰𝗸 𝗛𝗲𝗿𝗲 👈</a></b>\n\n<b>🛑You must Join Channels to access the links given below🛑\n\n<b><a href='https://t.me/joinchat/yC2Xo291HiZhMWM1'>𝗠𝗼𝘃𝗶𝗲 𝗖𝗵𝗮𝗻𝗻𝗲𝗹</a> ©\n<a href='https://t.me/joinchat/WQNEfDIqGDpkYzcx'>𝗦𝗲𝗿𝗶𝗲𝘀 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 </a>©</b>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
